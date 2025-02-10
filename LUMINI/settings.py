a4d543ce54f9c80e805668b54175e4b32410b25b
Francisco IALY <127478613+IALYFrancisco@users.noreply.github.com>
2025-02-10 15:56:33 +0300

    Update settings.py

--- LUMINI/settings.py
+++ LUMINI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3d)3ebdplx8^3^bc5xf&@y@oyd0xc#ew9pdybp=sj978o=s3ze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'https://lumini.onrender.com',

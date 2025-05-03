ad0fa47f7c7a7659693e1af8c222a3df729873a0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-03 12:41:47 +0300

    section marque de confiance, dynamisée

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+import requests
+import os
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    _headers = {
+        'x-api-key' : os.getenv('PROJECT_API_KEY')
+    }
+    response = requests.get(f"{os.getenv('API_URL')}", headers=_headers)
+    context = {
+        "projects" : response.json()["data"]
+    }
+    return render(request, 'index.html', context)

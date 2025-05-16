cffd8e6b6a35c1a6162c4322368deff802c0196f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-16 10:29:48 +0300

    debogage au niveau des urls

--- Home/urls.py
+++ Home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name="Home"),
-    path('/contact', views.contact, name="Contact"),
+    path('', views.index, name="Home"),
+    path('contact', views.contact, name="Contact"),
 ]

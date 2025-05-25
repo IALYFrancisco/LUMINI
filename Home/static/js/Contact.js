7119d7dcaedb9ae61aeae82792b0b136a3f188b6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-25 08:14:37 +0300

    dynamisation de la page de contact avec javascript

--- Home/static/js/Contact.js
+++ Home/static/js/Contact.js
@@ -1,10 +1,16 @@
 var formLeftAside = document.querySelector('.left')
+var reasonInput = document.querySelector('.reason')
+var numberPhone = document.querySelector('.number')
 
 function checkSelectValue(){
     let selectValue = document.querySelector('#object').value
     if(selectValue === "date"){
         formLeftAside.classList.add('active')
+        reasonInput.style.display = 'block'
+        numberPhone.style.display = 'block'
     }else{
         formLeftAside.classList.remove('active')
+        reasonInput.style.display = 'none'
+        numberPhone.style.display = 'none'
     }
 }

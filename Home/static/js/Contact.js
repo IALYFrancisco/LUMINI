4028ba8520ccf635bca0138b66becc9048f08a58
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-25 07:41:38 +0300

    précision de sélécteur

--- Home/static/js/Contact.js
+++ Home/static/js/Contact.js
@@ -1,7 +1,7 @@
 var formLeftAside = document.querySelector('.left')
 
 function checkSelectValue(){
-    let selectValue = document.querySelector('select').value
+    let selectValue = document.querySelector('#object').value
     if(selectValue === "date"){
         formLeftAside.classList.add('active')
     }else{

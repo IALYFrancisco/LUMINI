1d20c4b1f7d1880ce446afc0dfd6b31efeded54d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-30 05:49:07 +0300

    configuration du donnés ContactMessage

--- Home/models.py
+++ Home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -10,3 +12,9 @@ class ContactMessage(models.Model):
     message = models.CharField()
     type = models.CharField()
     register_date = models.DateField("Date d'envoie")
+
+    def __str__(self):
+        return self.message
+    
+    def was_registered_recently(self):
+        return self.register_date >= timezone.now() - datetime.timedelta(days=1)

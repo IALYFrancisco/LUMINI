382043f64a838b10421ee9cddf0fad41c712824d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-30 04:56:05 +0300

    définition du modèle de données ContactMessage pour la page de dontact

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactMessage(models.Model):
+    client_name = models.CharField(max_length=40)
+    client_phone = models.CharField(max_length=20)
+    client_email = models.CharField(max_length=50)
+    message_object = models.CharField()
+    message = models.CharField()
+    type = models.CharField()
+    register_date = models.DateField("Date d'envoie")

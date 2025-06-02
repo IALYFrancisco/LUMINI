6c1b8d5732646fe6205118f8fa1969852ddf0192
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-02 14:56:21 +0300

    création de migration et mis à jour de modèle

--- Home/models.py
+++ Home/models.py
@@ -8,9 +8,9 @@ class ContactMessage(models.Model):
     client_name = models.CharField(max_length=40)
     client_phone = models.CharField(max_length=20)
     client_email = models.CharField(max_length=50)
-    message_object = models.CharField()
-    message = models.CharField()
-    type = models.CharField()
+    message_object = models.CharField(max_length=500)
+    message = models.TextField()
+    type = models.CharField(max_length=500)
     register_date = models.DateField("Date d'envoie")
 
     def __str__(self):

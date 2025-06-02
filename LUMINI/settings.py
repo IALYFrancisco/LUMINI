c49c97238d7644e8c3c70032fcecc40a96e6df98
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-02 15:02:16 +0300

    recréation du fichier de migration

--- LUMINI/settings.py
+++ LUMINI/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
     }
 }
 
